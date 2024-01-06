5e49acbb5ed271743b501986438413b67207f0f4
Hideaki Narita <naritahideaki2010@docomo.ne.jp>
2024-01-06 21:58:28 +0900

    rename resource names for guid tab.

--- MyToolbox/GuidDialogBox.cpp
+++ MyToolbox/GuidDialogBox.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "GuidDialogBox.h"
+#include "MyToolbox.h"
 #include "resource.h"
 #include "hnrt/Menu.h"
 #include "hnrt/RegistryKey.h"
@@ -21,7 +22,7 @@ GuidDialogBox::GuidDialogBox()
     : MyDialogBox(IDD_GUID, L"Guid")
     , m_guid()
     , m_szFormatted()
-    , m_uCurrentlySelected(IDC_GUID_RADIO_UPPERCASE)
+    , m_uCurrentlySelected(IDC_GUID_UPPERCASE_RADIO)
 {
 }
 
@@ -29,13 +30,30 @@ GuidDialogBox::GuidDialogBox()
 void GuidDialogBox::OnCreate()
 {
     MyDialogBox::OnCreate();
+    ByteString initData;
     RegistryKey hKey;
     LSTATUS rc = hKey.Open(HKEY_CURRENT_USER, m_szRegistryKeyName, 0, KEY_READ);
     if (rc == ERROR_SUCCESS)
     {
-        m_uCurrentlySelected = RegistryValue::GetDWORD(hKey, REGVAL_FORMAT, 1) + IDC_GUID_RADIO_UPPERCASE - 1;
+        m_uCurrentlySelected = RegistryValue::GetDWORD(hKey, REGVAL_FORMAT, 1) + IDC_GUID_UPPERCASE_RADIO - 1;
+        try
+        {
+            initData = ByteString::FromHex(RegistryValue::GetSZ(hKey, REGVAL_LAST));
+        }
+        catch (...)
+        {
+        }
     }
-    ChangeGuid();
+    SetFont(IDC_GUID_RESULT_STATIC, GetApp<MyToolbox>().GetFontForData());
+    if (initData.Len == sizeof(m_guid))
+    {
+        memcpy_s(&m_guid, initData.Len, initData.Ptr, initData.Len);
+        ChangeFormat();
+    }
+    else
+    {
+        ChangeGuid();
+    }
     ButtonCheck(m_uCurrentlySelected);
     m_menuView
         .Add(ResourceString(IDS_MENU_GUID), IDM_VIEW_GUID);
@@ -48,20 +66,21 @@ void GuidDialogBox::OnDestroy()
     LSTATUS rc = hKey.Create(HKEY_CURRENT_USER, m_szRegistryKeyName, 0, KEY_WRITE);
     if (rc == ERROR_SUCCESS)
     {
-        RegistryValue::SetDWORD(hKey, REGVAL_FORMAT, m_uCurrentlySelected - IDC_GUID_RADIO_UPPERCASE + 1);
-        RegistryValue::SetSZ(hKey, REGVAL_LAST, m_szFormatted);
+        RegistryValue::SetDWORD(hKey, REGVAL_FORMAT, m_uCurrentlySelected - IDC_GUID_UPPERCASE_RADIO + 1);
+        RegistryValue::SetSZ(hKey, REGVAL_LAST, String::ToHex(&m_guid, sizeof(m_guid), UPPERCASE));
     }
+    SetFont(IDC_GUID_RESULT_STATIC, NULL);
     MyDialogBox::OnDestroy();
 }
 
 
 void GuidDialogBox::UpdateLayout(HWND hDlg, LONG cxDelta, LONG cyDelta)
 {
-    WindowLayout::UpdateLayout(hDlg, IDC_GUID_BUTTON_NEW, cxDelta, 0, 0, 0);
-    WindowLayout::UpdateLayout(hDlg, IDC_GUID_BUTTON_COPY, cxDelta, 0, 0, 0);
-    WindowLayout::UpdateLayout(hDlg, IDC_GUID_FORMAT_GROUPBOX, 0, 0, cxDelta, 0);
-    WindowLayout::UpdateLayout(hDlg, IDC_GUID_RESULT_GROUPBOX, 0, 0, cxDelta, cyDelta);
-    WindowLayout::UpdateLayout(hDlg, IDC_GUID_RESULT, 0, 0, cxDelta, cyDelta);
+    WindowLayout::UpdateLayout(hDlg, IDC_GUID_NEW_BUTTON, cxDelta, 0, 0, 0);
+    WindowLayout::UpdateLayout(hDlg, IDC_GUID_COPY_BUTTON, cxDelta, 0, 0, 0);
+    WindowLayout::UpdateLayout(hDlg, IDC_GUID_FORMAT_GROUP, 0, 0, cxDelta, 0);
+    WindowLayout::UpdateLayout(hDlg, IDC_GUID_RESULT_GROUP, 0, 0, cxDelta, cyDelta);
+    WindowLayout::UpdateLayout(hDlg, IDC_GUID_RESULT_STATIC, 0, 0, cxDelta, cyDelta);
 }
 
 
@@ -86,23 +105,35 @@ void GuidDialogBox::OnTabSelectionChanged()
 
 INT_PTR GuidDialogBox::OnCommand(WPARAM wParam, LPARAM lParam)
 {
-    switch (LOWORD(wParam))
+    UNREFERENCED_PARAMETER(lParam);
+    UINT idChild = LOWORD(wParam);
+    UINT idNotif = HIWORD(wParam);
+    switch (idChild)
     {
-    case IDC_GUID_BUTTON_COPY:
-        OnCopy();
+    case IDC_GUID_COPY_BUTTON:
+        if (idNotif == BN_CLICKED)
+        {
+            OnCopy();
+        }
         break;
-    case IDC_GUID_BUTTON_NEW:
-        ChangeGuid();
+    case IDC_GUID_NEW_BUTTON:
+        if (idNotif == BN_CLICKED)
+        {
+            ChangeGuid();
+        }
         break;
-    case IDC_GUID_RADIO_UPPERCASE:
-    case IDC_GUID_RADIO_LOWERCASE:
-    case IDC_GUID_RADIO_IMPLEMENT:
-    case IDC_GUID_RADIO_DEFINE:
-    case IDC_GUID_RADIO_STRUCT:
-    case IDC_GUID_RADIO_REGISTRY:
-    case IDC_GUID_RADIO_SQUARE:
-    case IDC_GUID_RADIO_ANGLE:
-        ChangeFormat(LOWORD(wParam));
+    case IDC_GUID_UPPERCASE_RADIO:
+    case IDC_GUID_LOWERCASE_RADIO:
+    case IDC_GUID_IMPLEMENT_RADIO:
+    case IDC_GUID_DEFINE_RADIO:
+    case IDC_GUID_STRUCT_RADIO:
+    case IDC_GUID_REGISTRY_RADIO:
+    case IDC_GUID_SQUARE_RADIO:
+    case IDC_GUID_ANGLE_RADIO:
+        if (idNotif == BN_CLICKED)
+        {
+            ChangeFormat(idChild);
+        }
         break;
     default:
         return FALSE;
@@ -136,8 +167,8 @@ void GuidDialogBox::ChangeGuid()
     else
     {
         m_szFormatted.Format(L"CoCreateGuid failed. (%08X)", (int)hRes);
-        SetText(IDC_GUID_RESULT, m_szFormatted);
-        DisableWindow(IDC_GUID_BUTTON_COPY);
+        SetText(IDC_GUID_RESULT_STATIC, m_szFormatted);
+        DisableWindow(IDC_GUID_COPY_BUTTON);
     }
 }
 
@@ -152,17 +183,17 @@ void GuidDialogBox::ChangeFormat(UINT uSelected)
     if (!StringFromGUID2(m_guid, buf, _countof(buf)))
     {
         m_szFormatted = L"StringFromGUID2 failed.";
-        SetText(IDC_GUID_RESULT, m_szFormatted);
-        DisableWindow(IDC_GUID_BUTTON_COPY);
+        SetText(IDC_GUID_RESULT_STATIC, m_szFormatted);
+        DisableWindow(IDC_GUID_COPY_BUTTON);
         return;
     }
     GUID& guid = m_guid;
     switch (m_uCurrentlySelected)
     {
-    case IDC_GUID_RADIO_LOWERCASE:
+    case IDC_GUID_LOWERCASE_RADIO:
         m_szFormatted.Assign(&buf[1], 36).Lowercase();
         break;
-    case IDC_GUID_RADIO_IMPLEMENT:
+    case IDC_GUID_IMPLEMENT_RADIO:
         /*
         * // {CC9CCEEF-195A-4539-9378-D083818A61C6}
         * IMPLEMENT_OLECREATE(<<class>>, <<external_name>>,
@@ -173,7 +204,7 @@ void GuidDialogBox::ChangeFormat(UINT uSelected)
             buf, guid.Data1, guid.Data2, guid.Data3,
             guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
         break;
-    case IDC_GUID_RADIO_DEFINE:
+    case IDC_GUID_DEFINE_RADIO:
         /*
         * // {CC9CCEEF-195A-4539-9378-D083818A61C6}
         * DEFINE_GUID(<<name>>,
@@ -184,7 +215,7 @@ void GuidDialogBox::ChangeFormat(UINT uSelected)
             buf, guid.Data1, guid.Data2, guid.Data3,
             guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
         break;
-    case IDC_GUID_RADIO_STRUCT:
+    case IDC_GUID_STRUCT_RADIO:
         /*
         * // {CC9CCEEF-195A-4539-9378-D083818A61C6}
         * static const GUID <<name>> =
@@ -195,29 +226,29 @@ void GuidDialogBox::ChangeFormat(UINT uSelected)
             buf, guid.Data1, guid.Data2, guid.Data3,
             guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
         break;
-    case IDC_GUID_RADIO_REGISTRY:
+    case IDC_GUID_REGISTRY_RADIO:
         /*
         * {CC9CCEEF-195A-4539-9378-D083818A61C6}
         */
         m_szFormatted = buf;
         break;
-    case IDC_GUID_RADIO_SQUARE:
+    case IDC_GUID_SQUARE_RADIO:
         /*
         * [Guid("CC9CCEEF-195A-4539-9378-D083818A61C6")]
         */
         m_szFormatted.Format(L"[Guid(\"%.36s\")]", & buf[1]);
         break;
-    case IDC_GUID_RADIO_ANGLE:
+    case IDC_GUID_ANGLE_RADIO:
         /*
         * <Guid("CC9CCEEF-195A-4539-9378-D083818A61C6")>
         */
         m_szFormatted.Format(L"<Guid(\"%.36s\")>", &buf[1]);
         break;
-    case IDC_GUID_RADIO_UPPERCASE:
+    case IDC_GUID_UPPERCASE_RADIO:
     default:
         m_szFormatted.Assign(&buf[1], 36);
         break;
     }
-    SetText(IDC_GUID_RESULT, m_szFormatted);
-    EnableWindow(IDC_GUID_BUTTON_COPY);
+    SetText(IDC_GUID_RESULT_STATIC, m_szFormatted);
+    EnableWindow(IDC_GUID_COPY_BUTTON);
 }
