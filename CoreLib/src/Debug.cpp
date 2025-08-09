f1c2f33f371554f71350b10ee55abe22c55b8162
Hideaki Narita <naritahideaki2010@docomo.ne.jp>
2025-08-09 17:21:54 +0900

    let debug use vc++ runtime only.

--- CoreLib/src/Debug.cpp
+++ CoreLib/src/Debug.cpp
@@ -1,10 +1,8 @@
 #include "pch.h"
 #include <stack>
+#include <string>
 #include <locale.h>
 #include "hnrt/Debug.h"
-#include "hnrt/Exception.h"
-#include "hnrt/StringBuffer.h"
-#include "hnrt/String.h"
 #include "hnrt/Buffer.h"
 
 
@@ -18,27 +16,27 @@ using namespace hnrt;
 ////////////////////////////////////////////////////////////////////////////
 
 
-typedef std::stack<String> FuncNameStack;
+typedef std::stack<std::wstring> FuncNameStack;
 
 
 static DWORD s_dwTlsIndex = TlsAlloc();
 
 
-static String GetFuncName()
+static const wchar_t* GetFuncName()
 {
     FuncNameStack* pStack = reinterpret_cast<FuncNameStack*>(TlsGetValue(s_dwTlsIndex));
     if (pStack && pStack->size())
     {
-        return pStack->top();
+        return pStack->top().c_str();
     }
     else
     {
-        return String::Empty;
+        return L"";
     }
 }
 
 
-static void PushFuncName(String pszFuncName)
+static void PushFuncName(const wchar_t* pszFuncName)
 {
     FuncNameStack* pStack = reinterpret_cast<FuncNameStack*>(TlsGetValue(s_dwTlsIndex));
     if (!pStack)
@@ -46,24 +44,24 @@ static void PushFuncName(String pszFuncName)
         pStack = new FuncNameStack();
         TlsSetValue(s_dwTlsIndex, pStack);
     }
-    pStack->push(pszFuncName);
+    pStack->push(std::wstring(pszFuncName));
 }
 
 
-static String PopFuncName()
+static void PopFuncName()
 {
     FuncNameStack* pStack = reinterpret_cast<FuncNameStack*>(TlsGetValue(s_dwTlsIndex));
     if (!pStack)
     {
-        throw Exception(L"Debug::PopFuncName: Null stack.");
+        OutputDebugStringW(L"Debug::PopFuncName: Null stack.");
+        return;
     }
     if (!pStack->size())
     {
-        throw Exception(L"Debug::PopFuncName: Empty stack.");
+        OutputDebugStringW(L"Debug::PopFuncName: Empty stack.");
+        return;
     }
-    String pszFuncName = pStack->top();
     pStack->pop();
-    return pszFuncName;
 }
 
 
@@ -85,17 +83,46 @@ void Debug::Put(PCWSTR pszFormat, ...)
 
 void Debug::VaPut(PCWSTR pszFormat, va_list argList)
 {
-    String buf = GetFuncName();
-    if (buf.Len)
+    static const wchar_t szLeaderFormat[] = L"%s: ";
+    const wchar_t* pszFuncName = GetFuncName();
+    int cch1 = *pszFuncName ? _scwprintf(szLeaderFormat, pszFuncName) : 0;
+    if (cch1 < 0)
     {
-        buf += L": ";
+        OutputDebugStringW(L"Debug::VaPut: _scwprintf failed.\n");
+        return;
     }
-    buf.VaAppendFormat(pszFormat, argList);
-    if (buf.Len && buf[buf.Len - 1] != L'\n')
+    va_list argList2;
+    va_copy(argList2, argList);
+    int cch2 = _vscwprintf(pszFormat, argList2);
+    va_end(argList2);
+    if (cch2 < 0)
     {
-        buf += L"\n";
+        OutputDebugStringW(L"Debug::VaPut: _vscwprintf failed.\n");
+        return;
     }
-    OutputDebugStringW(buf);
+    int cch = cch1 + cch2;
+    if (cch > 0)
+    {
+        Buffer<wchar_t> buf(cch + 2);
+        if (cch1 > 0)
+        {
+            _snwprintf_s(buf, buf.Len, _TRUNCATE, szLeaderFormat, pszFuncName);
+        }
+        if (cch2 > 0)
+        {
+            _vsnwprintf_s(buf + cch1, buf.Len - cch1, _TRUNCATE, pszFormat, argList);
+        }
+        if (buf[cch - 1] == L'\n')
+        {
+            buf[cch + 0] = L'\0';
+        }
+        else
+        {
+            buf[cch + 0] = L'\n';
+            buf[cch + 1] = L'\0';
+        }
+        OutputDebugStringW(buf);
+    }
 }
 
 
@@ -123,57 +150,83 @@ void Debug::Put(UINT cp, PCSTR pszFormat, ...)
 }
 
 
+static _locale_t CreateLocale(INT codepage)
+{
+    char szSpec[16] = { 0 };
+    _snprintf_s(szSpec, _TRUNCATE, codepage == CP_ACP ? ".ACP" : ".%d", codepage);
+    return _create_locale(LC_ALL, szSpec);
+}
+
+
+struct LocaleHelper
+{
+    _locale_t locale;
+
+    LocaleHelper(INT codepage)
+        : locale(CreateLocale(codepage))
+    {
+    }
+
+    ~LocaleHelper()
+    {
+        _free_locale(locale);
+    }
+};
+
+
 void Debug::VaPut(INT cp, PCSTR pszFormat, va_list argList)
 {
-    static _locale_t locale = _create_locale(LC_ALL, "C");
-    if (!*pszFormat)
+    static const char szLeaderFormat[] = "%S: ";
+    LocaleHelper h(cp);
+    const wchar_t* pszFuncName = GetFuncName();
+    int cb1 = *pszFuncName ? _scprintf(szLeaderFormat, pszFuncName) : 0;
+    if (cb1 < 0)
     {
+        OutputDebugStringW(L"Debug::VaPut: _scprintf failed.\n");
         return;
     }
-    int cb = _vscprintf_l(pszFormat, locale, argList);
-    if (cb < 1)
+    va_list argList2;
+    va_copy(argList2, argList);
+    int cb2 = _vscprintf_l(pszFormat, h.locale, argList2);
+    va_end(argList2);
+    if (cb2 < 0)
     {
-        OutputDebugStringW(L"Debug::VaPut: _vscprintf failed.\n");
+        OutputDebugStringW(L"Debug::VaPut: _vscprintf_l failed.\n");
         return;
     }
-    int size1 = cb + 1;
-    Buffer<CHAR> buf1(size1);
-    _vsnprintf_s_l(buf1, size1, _TRUNCATE, pszFormat, locale, argList);
-    int cch = MultiByteToWideChar(cp, MB_PRECOMPOSED, buf1, cb, NULL, 0);
-    if (cch < 1)
+    int cb = cb1 + cb2;
+    if (cb > 0)
     {
-        if (cp == CP_ACP)
+        Buffer<char> buf(cb + 2);
+        if (cb1 > 0)
         {
-            cch = MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, buf1, cb, NULL, 0);
+            _snprintf_s(buf, buf.Len, _TRUNCATE, szLeaderFormat, pszFuncName);
         }
-        if (cch < 1)
+        if (cb2 > 0)
         {
-            StringBuffer buf(128);
-            buf.Append(L"Debug::VaPut: MultiByteToWideChar failed:");
-            for (PCHAR pCur = buf1; *pCur; pCur++)
-            {
-                buf.AppendFormat(L" %02X", *pCur & 0xFF);
-            }
-            buf.Append(L"\n");
-            OutputDebugStringW(buf);
+            _vsnprintf_s_l(buf + cb1, buf.Len - cb1, _TRUNCATE, pszFormat, h.locale, argList);
+        }
+        if (buf[cb - 1] == '\n')
+        {
+            buf[cb + 0] = '\0';
+            cb++;
+        }
+        else
+        {
+            buf[cb + 0] = '\n';
+            buf[cb + 1] = '\0';
+            cb += 2;
+        }
+        int cch = MultiByteToWideChar(cp, MB_PRECOMPOSED, buf, cb, nullptr, 0);
+        if (cch < 0)
+        {
+            OutputDebugStringW(L"Debug::VaPut: MultiByteToWideChar failed.\n");
             return;
         }
+        Buffer<wchar_t> buf2(cch);
+        MultiByteToWideChar(cp, MB_PRECOMPOSED, buf, cb, buf2, cch);
+        OutputDebugStringW(buf2);
     }
-    int size2 = cch + 1;
-    Buffer<WCHAR> buf2(size2);
-    MultiByteToWideChar(cp, MB_PRECOMPOSED, buf1, cb, buf2, cch);
-    buf2[cch] = L'\0';
-    String buf = GetFuncName();
-    if (buf.Len)
-    {
-        buf += L": ";
-    }
-    buf += buf2;
-    if (buf.Len && buf[buf.Len - 1] != L'\n')
-    {
-        buf += L"\n";
-    }
-    OutputDebugStringW(buf);
 }
 
 
@@ -186,13 +239,35 @@ void Debug::VaPut(INT cp, PCSTR pszFormat, va_list argList)
 
 DebugFunc::DebugFunc(PCWSTR pszFormat, ...)
 {
-    StringBuffer buf(64);
-    buf.AppendFormat(L"%lu/", GetCurrentThreadId());
+    static const wchar_t szUnknownFunction[] = L"?";
+    static const wchar_t szThreadIdFormat[] = L"%lu/";
+    DWORD dwThreadId = GetCurrentThreadId();
+    int cch1 = _scwprintf(szThreadIdFormat, dwThreadId);
+    if (cch1 < 0)
+    {
+        OutputDebugStringW(L"DebugFunc::ctor: _scwprintf failed.\n");
+        PushFuncName(szUnknownFunction);
+        Debug::Put(L"Started.");
+        return;
+    }
+    va_list argList2;
+    va_start(argList2, pszFormat);
+    int cch2 = _vscwprintf(pszFormat, argList2);
+    va_end(argList2);
+    if (cch2 < 0)
+    {
+        OutputDebugStringW(L"DebugFunc::ctor: _vscwprintf failed.\n");
+        PushFuncName(szUnknownFunction);
+        Debug::Put(L"Started.");
+        return;
+    }
+    Buffer<wchar_t> buf(cch1 + cch2 + 1);
+    _snwprintf_s(buf, buf.Len, _TRUNCATE, szThreadIdFormat, dwThreadId);
     va_list argList;
     va_start(argList, pszFormat);
-    buf.VaAppendFormat(pszFormat, argList);
+    _vsnwprintf_s(buf + cch1, buf.Len - cch1, _TRUNCATE, pszFormat, argList);
     va_end(argList);
-    PushFuncName(buf.Detach());
+    PushFuncName(buf);
     Debug::Put(L"Started.");
 }
 
