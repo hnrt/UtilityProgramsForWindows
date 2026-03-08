ba0a7d8272805eaa53ca8f683eb4a92080e4e453
Hideaki Narita <naritahideaki2010@docomo.ne.jp>
2026-03-08 21:48:26 +0900

    fix String::set_Length.

--- CoreLib/src/String_ctor.cpp
+++ CoreLib/src/String_ctor.cpp
@@ -4,6 +4,7 @@
 #include "hnrt/StringCommons.h"
 #include "hnrt/RefString.h"
 #include "hnrt/RefStr.h"
+#include "hnrt/Interlocked.h"
 
 
 using namespace hnrt;
@@ -23,5 +24,34 @@ SIZE_T String::get_Length() const
 
 void String::set_Length(SIZE_T length)
 {
-    RefStr::Get(m_psz).Truncate(length);
+    if (m_psz)
+    {
+        if (length)
+        {
+            SIZE_T len0 = RefStr::Get(m_psz).Length;
+            if (len0 < length)
+            {
+                PWSTR psz0 = Interlocked<PWSTR>::ExchangePointer(&m_psz, RefStr::Create(length));
+                MemCpy(m_psz, psz0, len0);
+                MemSet(m_psz + len0, 0, length + 1 - len0);
+                StringRelease(psz0);
+            }
+            else if (length < len0)
+            {
+                PWSTR psz0 = Interlocked<PWSTR>::ExchangePointer(&m_psz, RefStr::Create(length));
+                MemCpy(m_psz, psz0, length);
+                m_psz[length] = L'\0';
+                StringRelease(psz0);
+            }
+        }
+        else
+        {
+            StringRelease(Interlocked<PWSTR>::ExchangePointer(&m_psz, nullptr));
+        }
+    }
+    else if (length)
+    {
+        m_psz = RefStr::Create(length);
+        MemSet(m_psz, 0, length + 1);
+    }
 }
